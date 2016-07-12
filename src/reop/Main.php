7bff8cdb03f4a992f2acd1c252f2d382c0b9bcb5
Michael <Mikeydoo4u@gmail.com>
2016-07-12 12:57:22 -0500

    Few changes to help development

--- src/reop/Main.php
+++ src/reop/Main.php
@@ -3,35 +3,43 @@ namespace reop;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\CommandExecutor;
-use pocketmine\event\Listener;
+use pocketmine\event\Listener as L;
 use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginBase as PB;
 use pocketmine\Server;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener{
+class Main extends PB implements L{
     
     public function onLoad(){
-        $this->getLogger()->info(TextFormat::GREEN . "REOP");
+        $this->getLogger()->info(TF::GREEN . "REOP");
     }
+
     public function onEnable(){
-        $this->getLogger()->info(TextFormat::GREEN . "REOP ENABLED");
+        @mkdir($this->getDataFolder());
+        $this->saveResource("ops.yml");
+        $Config = new Config($this->getdatafolder() . "ops.yml", Config::YAML);
+        $this->getLogger()->info(TF::GREEN . "REOP Enabled!");
+        $this->getServer()->registerEvents($this, $this);
     }
-    
+
     public function onDisable(){
+        $this->getLogger()->info(TF::RED. "REOP Disabled!");
     }
     
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        switch ($command->getName()){
-                $namesofpeople = $this->getConfig()->get("ReOp");
-            /**
-             *
-             *                                         LOVE
-             *
-             **/
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if(!$sender instanceof Player){
+            $sender->sendmessgase("Please use this command in-game");
+            return;
+        }
+
+        switch($cmd->getName()){
             case "reop":
-                $this->getServer()->dispatchCommand("op" . $namesofpeople);
+                $player = strtolower($sender->getName());
+                if($Config->exists($player));
+                $sender->sendmessage("Works");
+            }
         }
     }
 }
